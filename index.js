7b39d5350a2ff6e692424aa266032f93d305e3e2
Alejo Castaño Rojas <alejocas@experimentality.co>
2019-11-23 01:15:32 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { NuxtApp } = require('@keystonejs/app-nuxt');
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+
+const PROJECT_NAME = "Inventory Manager";
+
+
+/**
+ * You've got a new KeystoneJS Project! Things you might want to do next:
+ * - Add adapter config options (See: https://keystonejs.com/keystonejs/adapter-mongoose/)
+ * - Select configure access control and authentication (See: https://keystonejs.com/api/access-control)
+ */
+
+const keystone = new Keystone({
+  name: PROJECT_NAME,
+  adapter: new Adapter(),
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    new AdminUIApp({ enableDefaultRoute: true }),
+    new NuxtApp({
+      srcDir: 'src',
+      buildDir: 'dist',
+    })
+  ],
+};
